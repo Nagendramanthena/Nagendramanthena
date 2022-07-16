66575fc29a9ee6e1fa82b9a6299bc3c8325bbcbb
nagendravarma <manthenanagendra@gmail.com>
2022-07-16 18:07:27 +0530

    added greetings some more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("hello")
 from translate import Translator
-greetings = ["hello","goodmorning"]
+greetings = ["hello","goodmorning","how are you"]
 trans = Translator(to_lang="telugu")
 for g in greetings:
     print(trans.translate(g));
